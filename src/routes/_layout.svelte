71eee85d3e0b89c1613210a2c2d8ab9b1e54821a
Neef Rehman <neef.rehman@gmail.com>
2020-08-24 12:12:06 +0100

    globe rotation

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,14 +1,39 @@
+<script>
+    import { onMount } from "svelte";
+
+    const globeEmojis = ["🌍", "🌎", "🌏"];
+    let globeEmojiIndex = 0;
+    let currentGlobeEmoji = globeEmojis[globeEmojiIndex];
+
+    const rotateGlobeEmoji = () => {
+        if (document.hasFocus()) {
+            globeEmojiIndex = globeEmojiIndex < 2 ? globeEmojiIndex + 1 : 0;
+            currentGlobeEmoji = globeEmojis[globeEmojiIndex];
+        }
+    };
+
+    onMount(() => {
+        setInterval(rotateGlobeEmoji, 800);
+        return () => clearInterval(rotateGlobeEmoji);
+    });
+</script>
+
 <style>
-    main {
-        position: relative;
-        max-width: 56em;
-        background-color: white;
+    :global(body) {
+        background-color: #f1f1f1;
+        color: #101010;
+        max-width: 1180px;
         padding: 2em;
         margin: 0 auto;
-        box-sizing: border-box;
     }
 </style>
 
-<main>
-    <slot />
-</main>
+<svelte:head>
+    <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22
+        viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>{currentGlobeEmoji}</text></svg>" />
+    <title>personalbrand.club</title>
+</svelte:head>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,42 +6,24 @@
     }
 
     h1 {
-        font-size: 2.8em;
-        text-transform: uppercase;
-        font-weight: 700;
+        font-size: 4em;
+        font-weight: 400;
         margin: 0 0 0.5em 0;
-    }
-
-    img {
-        width: 100%;
-        max-width: 400px;
-        margin: 0 0 1em 0;
     }
 
     p {
         margin: 1em auto;
     }
-
-    @media (min-width: 480px) {
-        h1 {
-            font-size: 4em;
-        }
-    }
 </style>
 
-<svelte:head>
-    <title>Sapper project template</title>
-</svelte:head>
+<header>
+    <h1>personalbrand</h1>
+</header>
 
-<h1>personalbrand</h1>
+<p>info</p>
 
-<figure>
-    <img alt="Success Kid" src="successkid.jpg" />
-    <figcaption>Have fun with Sapper!</figcaption>
-</figure>
+<p>tracks</p>
 
-<p>
-    <strong>
-        Try editing this file (src/routes/index.svelte) to test live reloading.
-    </strong>
-</p>
+<footer>
+    <p>footer</p>
+</footer>
